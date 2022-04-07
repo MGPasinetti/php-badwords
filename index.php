98b20bfa12cfb4f3170e07c9d41515bd77e408a2
MGPasinetti <mg.pasinetti@gmail.com>
2022-04-07 15:31:07 +0200

    paragraph and its length printed

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<!-- 
+Esercizio di oggi: PHP Badwords
+nome repo: php-badwords
+Descrizione:
+Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall'utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. 
+-->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +17,10 @@
     <title>PHP Badwords</title>
 </head>
 <body>
-    
+    <?php 
+        $string = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim a recusandae illum ipsam magnam exercitationem quia id ut voluptatem veritatis. Soluta, ipsa. Exercitationem debitis laboriosam quod voluptatibus molestiae repudiandae reiciendis.";
+    ?>
+
+    <p><?php echo $string . "\n" . strlen($string); ?></p>
 </body>
 </html>
